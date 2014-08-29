b04a058fd176f1104fa311366442144df3d7dae8
geniusvil <violet.rost@gmail.com>
2014-08-29 18:30:59 +0300

    SqlModel Created

---
+++ Databases-Teamwork-Practical-Project/SantasToyFactory/SantasToyFactory.DataLayer/Delivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SantasToyFactory.DataLayer
+{
+
+    public class Delivery
+    {
+
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public virtual ICollection<Vendor> Vendors { get; set; }
+
+    }
+}

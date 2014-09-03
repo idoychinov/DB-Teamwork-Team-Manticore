de8666f74ff16fa8521049a423c3d0dbbc7656c8
svilenbonev <svilenbonev@users.noreply.github.com>
2014-09-04 00:58:08 +0300

    added XML with additional information for children

---
+++ Databases-Teamwork-Practical-Project/SantasToyFactory/SantasToyFactory.ConsoleClient/XMLReader.cs
@@ -0,0 +1,50 @@
+﻿namespace SantasToyFactory.ConsoleClient
+{
+    using System;
+    using System.Xml;
+
+    using SantasToyFactory.DataLayer;
+
+    public class XMLReader
+    {
+        public XMLReader(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+
+        public string FilePath { get; set; }
+
+        public void LoadBehaviorData()
+        {
+            var db = new SantasToyFactoryDatabase();
+            using (XmlReader reader = XmlReader.Create("../../Children-Behaviors.xml"))
+            {
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) &&
+                        (reader.Name == "child"))
+                    {
+                        string childName = reader.GetAttribute("name");
+                        do
+                        {
+                            reader.Read();
+                        } while (reader.NodeType != "behavior");
+
+                        
+                    }
+                }
+            }
+            
+            using (XmlReader reader = XmlReader.Create("../../library.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        Console.WriteLine(reader.Name);
+                    }
+                }
+            }
+        }
+    }
+}
